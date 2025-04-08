64327cd0b873a10a9a11485f02746a8f6d10b635
Sudheesh406 <sudheeshunni406@gmail.com>
2025-04-08 12:15:38 +0530

    deployment

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const path = require('path'); 
-
 const cookieParser = require('cookie-parser');
 const loginRouter = require('./routers/loginRoute')
 const PackageRouter = require('./routers/TravelPackageRoute')
@@ -23,8 +21,6 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static(path.join(__dirname, './FRONT-END/build')))
-
 app.use('/',loginRouter)
 app.use('/Package',PackageRouter)
 app.use('/Company',companyRouter)
@@ -34,10 +30,6 @@ app.use('/Review',reviewRouter)
 app.use('/Admin',adminRouter)
 
 databaseCn();
-
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, './FRONT-END/build', 'index.html'));
- });
 
 app.listen(process.env.PORT,(err)=>{
     if(err){
