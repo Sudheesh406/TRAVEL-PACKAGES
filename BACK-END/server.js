0cf915378109abcefe0201a10be70a8527860118
Sudheesh406 <sudheeshunni406@gmail.com>
2025-04-23 11:57:39 +0530

    all commited

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -33,11 +33,11 @@ app.use('/api/Admin',adminRouter)
 const path = require('path');
 
 // Serve React frontend
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'FRONT-END/build')));
 
 // For any route not handled by the API, send back React's index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'FRONT-END/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../FRONT-END', 'index.html'));
 });
 
 databaseCn();
